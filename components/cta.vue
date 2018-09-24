b4ad086b792d0ef47dadda44e192da8a9589e5b5
Anand Chowdhary <anandchowdhary@gmail.com>
2018-09-24 19:05:12 +0200

    Data from json

---
+++ components/cta.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="cta">
+		<nuxt-link v-for="(button, index) in context.buttons" :key="`button_${index}`" :to="button.href" class="btn btn-dark mr-3" v-html="button.text" />
+	</div>
+</template>
+
+<script>
+export default {
+	props: ["context"]
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,9 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-8">
-						<h1>Technology for the next billion users.</h1>
-						<p style="max-width: 500px">Oswald Labs is an award-winning accessibility technology company. With our research-driven and inclusive product portfolio, we’ve impacted thousands of lives throughout the globe.</p>
-						<div class="cta">
-							<a href="#" class="btn btn-dark">Learn more</a>
-							<a href="#" class="btn btn-dark">Get in touch</a>
-						</div>
+						<h1>{{data.header.title}}</h1>
+						<p style="max-width: 500px" class="mt-4 mb-4">{{data.header.intro}}</p>
+						<cta :context="data.header" />
 					</div>
 				</div>
 			</div>
@@ -18,14 +15,28 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-8">
-						<h2>Platform</h2>
-						<p style="max-width: 500px">Oswald Labs Platform is a set of free and open APIs to build scalable, accessible applications. Get started with cross-platform SDKs featuring flexible APIs and exclusive tools.</p>
-						<div class="cta">
-							<a href="#" class="btn btn-light">Start building &rarr;</a>
-						</div>
+						<h2>{{data.new.title}}</h2>
+						<p style="max-width: 500px" class="mt-4 mb-4">{{data.new.intro}}</p>
+						<cta :context="data.new" />
 					</div>
 				</div>
 			</div>
 		</section>
 	</main>
 </template>
+
+<script>
+import axios from "axios";
+import cta from "../components/cta.vue";
+export default {
+	asyncData ({ params }) {
+		return axios.get(`http://localhost:3000/data/index.json`)
+		.then(res => {
+			return { data: res.data }
+		})
+	},
+	components: {
+		cta
+	}
+}
+</script>
