6d76ab9a9b4d4c0e57677eb0eef1580fff99c8aa
Anand Chowdhary <anandchowdhary@gmail.com>
2018-09-24 16:15:37 +0200

    Nuxt

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        oswaldlabs.com-vue
+      </h1>
+      <h2 class="subtitle">
+        My prime Nuxt.js project
+      </h2>
+    </div>
+  </section>
+</template>
---
+++ pages/team/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <section class="container">
+    TEAM Home
+  </section>
+</template>
