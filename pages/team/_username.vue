fbd97fe920dc5e41074c4c7e67b2f1ba437838e4
Anand Chowdhary <anandchowdhary@gmail.com>
2018-09-24 16:22:43 +0200

    Axios

--- pages/team/_username.vue
+++ pages/team/_username.vue
@@ -1,5 +1,17 @@
 <template>
-  <section class="container">
-    TEAM Single
-  </section>
+	<section class="container">
+		TEAM Single {{users}}
+	</section>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+	asyncData ({ params }) {
+		return axios.get(`https://reqres.in/api/users?page=2`)
+		.then(res => {
+			return { users: res.data }
+		})
+	}
+}
+</script>
